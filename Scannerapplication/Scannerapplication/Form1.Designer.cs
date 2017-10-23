5a29e09dc877023f87d8cd3a195cd1810227d722
Intesar Alam <alam.kir@gmail.com>
2017-10-23 21:48:56 +0530

    23102017

---
+++ Scannerapplication/Scannerapplication/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿using System.Windows.Forms;
+namespace Scannerapplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.showImages = new System.Windows.Forms.Button();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.pnl_capture = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pic_scan = new System.Windows.Forms.PictureBox();
+            this.lbDevices = new System.Windows.Forms.ListBox();
+            this.btn_scan = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.pnl_capture.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_scan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // showImages
+            // 
+            this.showImages.Location = new System.Drawing.Point(242, 216);
+            this.showImages.Name = "showImages";
+            this.showImages.Size = new System.Drawing.Size(85, 23);
+            this.showImages.TabIndex = 5;
+            this.showImages.Text = "Show Images";
+            this.showImages.UseVisualStyleBackColor = true;
+            this.showImages.Visible = false;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(787, 3);
+            this.splitter1.TabIndex = 38;
+            this.splitter1.TabStop = false;
+            // 
+            // pnl_capture
+            // 
+            this.pnl_capture.BackColor = System.Drawing.Color.Transparent;
+            this.pnl_capture.Controls.Add(this.lblStatus);
+            this.pnl_capture.Controls.Add(this.progressBar1);
+            this.pnl_capture.Controls.Add(this.button1);
+            this.pnl_capture.Controls.Add(this.label2);
+            this.pnl_capture.Controls.Add(this.label1);
+            this.pnl_capture.Controls.Add(this.pic_scan);
+            this.pnl_capture.Controls.Add(this.lbDevices);
+            this.pnl_capture.Controls.Add(this.btn_scan);
+            this.pnl_capture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_capture.Location = new System.Drawing.Point(0, 3);
+            this.pnl_capture.Name = "pnl_capture";
+            this.pnl_capture.Size = new System.Drawing.Size(787, 712);
+            this.pnl_capture.TabIndex = 39;
+            this.pnl_capture.Paint += new System.Windows.Forms.PaintEventHandler(this.pnl_capture_Paint);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(624, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Capture Screen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(621, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(621, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // pic_scan
+            // 
+            this.pic_scan.BackColor = System.Drawing.Color.White;
+            this.pic_scan.Location = new System.Drawing.Point(165, 21);
+            this.pic_scan.Name = "pic_scan";
+            this.pic_scan.Size = new System.Drawing.Size(450, 600);
+            this.pic_scan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_scan.TabIndex = 6;
+            this.pic_scan.TabStop = false;
+            this.pic_scan.Visible = false;
+            // 
+            // lbDevices
+            // 
+            this.lbDevices.FormattingEnabled = true;
+            this.lbDevices.Location = new System.Drawing.Point(21, 53);
+            this.lbDevices.Name = "lbDevices";
+            this.lbDevices.Size = new System.Drawing.Size(120, 30);
+            this.lbDevices.TabIndex = 5;
+            this.lbDevices.Visible = false;
+            // 
+            // btn_scan
+            // 
+            this.btn_scan.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_scan.Location = new System.Drawing.Point(21, 17);
+            this.btn_scan.Name = "btn_scan";
+            this.btn_scan.Size = new System.Drawing.Size(75, 30);
+            this.btn_scan.TabIndex = 4;
+            this.btn_scan.Text = "Scan";
+            this.btn_scan.UseVisualStyleBackColor = true;
+            this.btn_scan.Click += new System.EventHandler(this.btn_scan_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(165, 627);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(450, 23);
+            this.progressBar1.TabIndex = 12;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblStatus.Location = new System.Drawing.Point(162, 653);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblStatus.TabIndex = 13;
+            this.lblStatus.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(787, 715);
+            this.Controls.Add(this.pnl_capture);
+            this.Controls.Add(this.splitter1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Scanner Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Home_SizeChanged);
+            this.pnl_capture.ResumeLayout(false);
+            this.pnl_capture.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_scan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+
+        //camera
+        private System.Windows.Forms.Button showImages;
+        private Splitter splitter1;
+        private Panel pnl_capture;
+        private PictureBox pic_scan;
+        private ListBox lbDevices;
+        private Button btn_scan;
+        private Label label2;
+        private Label label1;
+        private Button button1;
+        private ProgressBar progressBar1;
+        private Label lblStatus;
+    }
+}
+
---
+++ Scannerapplication/Scannerapplication/Form1.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using WIATest;
+
+namespace Scannerapplication
+{
+    public partial class Form1 : Form
+    {
+        private const int BufferSize = 1024;
+        public string hostip = string.Empty;
+        public string hostport = string.Empty;
+        public string dbserver = string.Empty;
+        public string dbuser = string.Empty;
+        public string dbpassword = string.Empty;
+        public string dbname = string.Empty;
+        public string tablename = string.Empty;
+        public string fieldname = string.Empty;
+        public string keyfield = string.Empty;
+        public string keyvalue = string.Empty;
+        public int isinsert = 0; 
+        public int isdb = 0;
+        public Image ImageScanned=null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        public void loadSetting()
+        {
+
+            try
+            {
+                string[] arguments = Environment.GetCommandLineArgs();
+                foreach (string str in arguments)
+                {
+                    if(str.Contains("OpenForm"))
+                    {
+                        string[] strarr = str.Split('=');
+                        if(strarr.Length>0)
+                        {
+                            keyvalue = strarr[1].ToString();
+                           // MessageBox.Show("Key Value"+ keyvalue);
+                        }
+                    }
+                    //MessageBox.Show(str);
+                    //keyvalue = "111";
+                }
+                string path = Path.Combine(Application.StartupPath, "hostsetting.txt");
+                if (File.Exists(path))
+                {
+                    using (StreamReader sr = File.OpenText(path))
+                    {
+                        string s = "";
+                        while ((s = sr.ReadLine()) != null)
+                        {
+                            string[] strarr = s.Split('|');
+                            if (strarr.Length >= 2)
+                            {
+                                hostip = strarr[0];
+                                hostport = strarr[1];
+                                dbserver = strarr[2];
+                                dbname = strarr[3];
+                                dbuser = strarr[4];
+                                dbpassword = strarr[5];
+                                isdb = Convert.ToInt32(strarr[6]);
+                                tablename = strarr[7];
+                                fieldname = strarr[8];
+                                keyfield = strarr[9];
+                                isinsert = Convert.ToInt32(strarr[6]);
+                                label1.Text = "Host: " + hostip;
+                                label2.Text = "Port: " + hostport;
+                                int port = 0;
+                                if (hostport.Length > 0 && hostip.Length > 0)
+                                {
+                                    try
+                                    {
+                                        port = Convert.ToInt32(hostport);
+                                        bool issend = sendData(hostip, port, path);
+                                        if (issend)
+                                        {
+                                            Application.Exit();
+                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        MessageBox.Show("Message : " + ex.ToString());
+                                    }
+
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Host IP or Port not found");
+                                }
+
+                            }
+                            else
+                            {
+                                MessageBox.Show("Setting Parameters are not correct: " + s);
+                            }
+                        }
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Host Setting Not Found at Location: " + path);
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+        public bool sendData(string hostaddr, int portaddr, string path)
+        {
+            string pathfile = Path.Combine(Path.GetDirectoryName(path), "scannedimg.jpeg");
+        
+            if(isdb==1)
+            {
+                UpdateScannedImage(ImageScanned );
+            }
+            else
+            {
+                ImageScanned.Save(pathfile);
+                if (File.Exists(pathfile))
+                {
+                    SendTCP(pathfile, hostaddr, portaddr);
+                }
+                else
+                {
+                    MessageBox.Show("Unable to Send to TCP: " + pathfile);
+                    return false;
+                }
+            }
+            return true;
+        }
+        public void SendTCP(string M, string IPA, Int32 PortN)
+        {
+
+            
+                byte[] SendingBuffer = null;
+                TcpClient client = null;
+                lblStatus.Text = "";
+                NetworkStream netstream = null;
+                try
+                {
+                    client = new TcpClient(IPA, PortN);
+                    lblStatus.Text = "Connected to the Server...\n";
+                    netstream = client.GetStream();
+                    FileStream Fs = new FileStream(M, FileMode.Open, FileAccess.Read);
+                    int NoOfPackets = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(Fs.Length) / Convert.ToDouble(BufferSize)));
+                    progressBar1.Maximum = NoOfPackets;
+                int TotalLength = (int)Fs.Length, CurrentPacketLength;
+                    for (int i = 0; i < NoOfPackets; i++)
+                    {
+                        if (TotalLength > BufferSize)
+                        {
+                            CurrentPacketLength = BufferSize;
+                            TotalLength = TotalLength - CurrentPacketLength;
+                        }
+                        else
+                            CurrentPacketLength = TotalLength;
+                        SendingBuffer = new byte[CurrentPacketLength];
+                        Fs.Read(SendingBuffer, 0, CurrentPacketLength);
+                        netstream.Write(SendingBuffer, 0, (int)SendingBuffer.Length);
+                        if (progressBar1.Value >= progressBar1.Maximum)
+                            progressBar1.Value = progressBar1.Minimum;
+                        progressBar1.PerformStep();
+                    }
+
+                    lblStatus.Text = lblStatus.Text + "Sent " + Fs.Length.ToString() + " bytes to the server";
+                    Fs.Close();
+                    netstream.Close();
+                    client.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+
+
+                }
+
+            
+            
+        }
+
+        public bool UpdateScannedImage(Image img)
+        {
+            if(keyvalue.Length==0)
+            {
+                MessageBox.Show("Unable to get Key Field Value.");
+                return false;
+            }
+            byte[] imgdata = getImage(img);
+            String str = "Data Source="+dbserver+";Initial Catalog="+dbname+";Integrated Security=False;User Id="+dbuser+";Password="+dbpassword+";MultipleActiveResultSets=True";
+            String query = "";
+            if(isinsert==1)
+            {
+                query = "insert into " + tablename + " ("+ keyfield + ","+fieldname+") values(@" + keyfield + ",@" + fieldname +")";
+            }
+            else
+            {
+                query = "Update " + tablename + " set " + fieldname + "=@" + fieldname + " where " + keyfield + "='" + keyvalue + "'";
+
+            }
+            try
+            {
+                SqlConnection con = new SqlConnection(str);
+                con.Open();
+                
+                SqlCommand cmd = new SqlCommand(query, con);
+                if(isinsert == 1)
+                {
+                    cmd.Parameters.Add(new SqlParameter("@" + keyfield, keyvalue));
+                }
+                cmd.Parameters.Add(new SqlParameter("@"+ fieldname, imgdata));
+                int row=cmd.ExecuteNonQuery();
+                con.Close();
+                if (row==0)
+                {
+                    throw new Exception("Unable to Save to DataBase.");
+                }
+                return true;
+            }
+            catch (Exception es)
+            {
+                MessageBox.Show(es.Message);
+            }
+
+            return false;
+        }
+public byte[] getImage(Image img)
+{
+    ImageConverter converter = new ImageConverter();
+    return (byte[])converter.ConvertTo(img, typeof(byte[]));
+}
+//button click event
+private void btn_scan_Click(object sender, EventArgs e)
+{
+    try
+    {
+
+
+        //get list of devices available
+        List<string> devices = WIAScanner.GetDevices();
+
+        foreach (string device in devices)
+        {
+            lbDevices.Items.Add(device);
+        }
+        //check if device is not available
+        if (lbDevices.Items.Count == 0)
+        {
+            MessageBox.Show("You do not have any WIA devices.");
+            DialogResult dr = MessageBox.Show("Would you like to Capture Screen?", "Alert", MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+            {
+                CaptureScrn();
+                loadSetting();
+                return;
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+        else
+        {
+            lbDevices.SelectedIndex = 0;
+        }
+        //get images from scanner
+        List<Image> images = WIAScanner.Scan((string)lbDevices.SelectedItem);
+        foreach (Image image in images)
+        {
+            pic_scan.Image = image;
+                    ImageScanned = image;
+            pic_scan.Show();
+            pic_scan.SizeMode = PictureBoxSizeMode.StretchImage;
+            //save scanned image into specific folder
+            // image.Save(@"D:\" + DateTime.Now.ToString("yyyy-MM-dd HHmmss") + ".jpeg", ImageFormat.Jpeg);
+        }
+        loadSetting();
+    }
+    catch (Exception exc)
+    {
+        MessageBox.Show(exc.Message);
+    }
+}
+
+
+private void Home_SizeChanged(object sender, EventArgs e)
+{
+    int pheight = this.Size.Height - 153;
+    pic_scan.Size = new Size(pheight - 150, pheight);
+}
+
+private void pnl_capture_Paint(object sender, PaintEventArgs e)
+{
+
+}
+
+private void Form1_Load(object sender, EventArgs e)
+{
+    btn_scan_Click(null, null);
+
+}
+
+private void button1_Click(object sender, EventArgs e)
+{
+    this.WindowState = FormWindowState.Minimized;
+    if (this.WindowState == FormWindowState.Minimized)
+    {
+        CaptureScrn();
+    }
+
+}
+public void CaptureScrn()
+{
+    try
+    {
+        //Creating a new Bitmap object
+        Bitmap captureBitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+
+        //Bitmap captureBitmap = new Bitmap(int width, int height, PixelFormat);
+        //Creating a Rectangle object which will  
+        //capture our Current Screen
+        Rectangle captureRectangle = Screen.AllScreens[0].Bounds;
+
+        //Creating a New Graphics Object
+        Graphics captureGraphics = Graphics.FromImage(captureBitmap);
+
+        //Copying Image from The Screen
+        captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
+
+        //Saving the Image File (I am here Saving it in My E drive).
+
+        //captureBitmap.Save(@"E:\Capture.jpg", ImageFormat.Jpeg);
+
+        pic_scan.InitialImage = null;
+        pic_scan.Image = (Image)captureBitmap;
+                ImageScanned= (Image)captureBitmap;
+                pic_scan.Show();
+        pic_scan.SizeMode = PictureBoxSizeMode.StretchImage;
+        this.WindowState = FormWindowState.Maximized;
+    }
+    catch (Exception ex)
+    {
+        MessageBox.Show(ex.ToString());
+    }
+    //Console.WriteLine("DEBUG: Server--> Entra nel While");
+    //ScreenCapture sc = new ScreenCapture(); // capture entire screen
+    //img = sc.CaptureScreen();
+    //img1 = (Image)img.Clone();
+    //padre.setImage(img1);
+    //if (img != null) //If you choosed an image
+    //{
+    //videoServer.SendImage(img); //Send the image
+
+
+}
+    }
+}
